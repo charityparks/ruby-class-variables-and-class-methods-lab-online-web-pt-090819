dff4608b92c54c27b88ef7872ebc44d17de0d56f
Charity Parks <tuppercharity@gmail.com>
2019-09-29 20:00:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,8 @@
 class Song 
-  @@count = 0 
   
-  attr_accessor :name, :artist, :genre
-    @@cou
+  attr_reader :name, :artist, :genre
+    @@count = 0 
+    
  
   def initialize 
     @@count += 1 
