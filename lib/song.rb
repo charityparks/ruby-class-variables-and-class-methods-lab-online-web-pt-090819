563ca5244b4c627a478d99646effdd1c23a8c5d4
Charity Parks <tuppercharity@gmail.com>
2019-09-29 20:03:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,9 +8,10 @@ class Song
   def initialize (name, artist, genre)
     @name = name
     @artist = artist
-    
-    
+    @genre = genre
     @@count += 1 
+    @artist << artist
+    
   end
   
   def self.count
